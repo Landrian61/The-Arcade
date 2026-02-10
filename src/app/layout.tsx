6f1605f6365029b3ecc6eac622111c6d99e10f8e
chrisppa <cturyasiima@gmail.com>
2026-02-10 18:03:59 +0300

    feat: add and configure tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,26 @@
-import type { Metadata } from 'next'
-import ThemeRegistry from '@/theme/registry'
-import './globals.css'
+import type { Metadata } from "next";
+import ThemeRegistry from "@/theme/registry";
+import "./globals.css";
+import QueryProvider from "@/lib/query-provider";
 
 export const metadata: Metadata = {
-  title: 'The Arcade - Frontend Playground',
-  description: 'A shared sandbox for the frontend team to experiment and create',
-}
+  title: "The Arcade - Frontend Playground",
+  description:
+    "A shared sandbox for the frontend team to experiment and create",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <ThemeRegistry>
-          {children}
-        </ThemeRegistry>
+        <QueryProvider>
+          <ThemeRegistry>{children}</ThemeRegistry>
+        </QueryProvider>
       </body>
     </html>
-  )
+  );
 }
-
-
-
-
-
-
-
